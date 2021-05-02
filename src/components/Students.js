c5f902f7efb2e18c0d8acf9ebafe341a2f504c9f
stefan <stefansgit@gmail.com>
2021-05-02 15:37:16 +0200

    without validation

---
+++ src/components/Students.js
@@ -0,0 +1,44 @@
+import { list } from '../services/apiService';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Students = () => {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        list('students', data => {
+            setStudents(data);
+        })
+    }, []);
+
+    return(
+        <div className='container'>
+            <h1>Students</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Year of Birth</th>
+                        <td><Link to='/students/0'>Add new</Link></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {students.map(s => (
+                        <tr key={s._id}>
+                            <td>{s.firstName}</td>
+                            <td>{s.lastName}</td>
+                            <td>{s.yearOfBirth}</td>
+                            {/* <td>{s.address}</td> */}
+                            <td>
+                                <Link to={`/students/${s._id}`}>Edit</Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Students;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import './assets/styles/site.css';
+import './assets/styles/table.css';
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
